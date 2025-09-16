790693b1bb2b8df6771bb04bd1496f7a8403b05d
Juno <sojae0306@gmail.com>
2025-09-16 16:46:18 +0900

    Add week 1 and week 2 files for data analysis and model training

--- week1.py
+++ week1.py
@@ -1,14 +1,13 @@
 import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+import tensorflow as tf
 
 Data_set = np.loadtxt("./data/ThoraricSurgery3.csv", delimiter=",")
 x = Data_set[:, 0:16]
 y = Data_set[:, 16]
 
-model = Sequential()
-model.add(Dense(30, input_dim=16, activation="relu"))
-model.add(Dense(1, activation="sigmoid"))
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(30, input_dim=16, activation="relu"))
+model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
 
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 history = model.fit(x, y, epochs=5, batch_size=16)
